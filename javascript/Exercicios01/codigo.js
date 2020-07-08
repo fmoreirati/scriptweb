28eafa34cb4fedfa2b011bc2e6860cc13350c5bf
fmoreirati <fabianomoreira.ti@gmail.com>
2020-07-08 09:31:18 -0300

    Aula 05

--- javascript/Exercicios01/codigo.js
+++ javascript/Exercicios01/codigo.js
@@ -1,5 +1,39 @@
 var alunos = [];
 
+//Questão 5a
+function resultadoOpt(nome, nota1, nota2, opc) {
+    let media = 0;
+    let texto = "";
+    //Nota optativa
+    if (opc != 0) {
+        if (nota1 < 6) {
+            nota1 = opc;
+        } else if (nota2 < 6) {
+            nota2 = opc;
+        }
+    }
+    //Calulo da nova media
+    media = ((nota1 + nota2) / 2).toFixed(2);
+    texto = "<span class='alert alert-info'>" + nome + ". Sua média : " + media + ". ";
+    if (media >= 6){
+        texto += "Aprovado </span>";
+    } else if (media < 3){
+        texto += "Reprovado </span>";
+    } else {
+        texto += "Em exame </span>";
+    }
+    addAluno({
+        nome: nome,
+        n1: nota1,
+        n2: nota2,
+        media: media
+    });
+    saida(texto + "<br>" + listAluno());
+}
+
+
+
+//Questao 5
 function resultado(nome, nota1, nota2) {
     let media = ((nota1 + nota2) / 2).toFixed(2);
     let texto = "<span class='alert alert-info'>" + nome + ". Sua média : " + media + ". ";
@@ -10,7 +44,7 @@ function resultado(nome, nota1, nota2) {
     }
     //document.getElementById("saida").innerHTML = texto
     //saida(texto);
-    addAluno({ 
+    addAluno({
         nome: nome,
         n1: nota1,
         n2: nota2,
@@ -20,17 +54,17 @@ function resultado(nome, nota1, nota2) {
 }
 
 function addAluno(aluno) {
-    alunos[alunos.length] = aluno
+    alunos[alunos.length] = aluno;
     console.log(alunos);
 }
 
 function listAluno() {
-    let texto = "<table class='table'>"
+    let texto = "<table class='table'>";
     for (let x = 0; x < alunos.length; x++) {
         texto += "<tr> <td>" + alunos[x].nome + "</td> <td>" + alunos[x].media + "</td> </tr>";
     }
     texto += "</table>";
-    return texto
+    return texto;
 }
 
 function saida(msg) {
