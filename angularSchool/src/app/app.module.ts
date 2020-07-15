1f51c49b1d674262604b4c6706f2c369291e911f
fmoreirati <fabianomoreira.ti@gmail.com>
2020-07-15 11:07:01 -0300

    Aula 08

--- angularSchool/src/app/app.module.ts
+++ angularSchool/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNavComponent } from './pages/page-nav/page-nav.component';
 import { PageHeaderComponent } from './pages/page-header/page-header.component';
 import { PageFooterComponent } from './pages/page-footer/page-footer.component';
+import { FormUsuarioComponent } from './pages/form-usuario/form-usuario.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNavComponent,
     PageHeaderComponent,
-    PageFooterComponent
+    PageFooterComponent,
+    FormUsuarioComponent,
+    PageHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
